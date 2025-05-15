8044f66a9bbf5045187d14e60b59f8471f549b1d
Bernardo Souza Muniz <be060890@intelbras.com.br>
2025-05-15 17:05:26 -0300

    Criação da classe login

---
+++ app/src/main/java/ifsc/poo/login/Register.java
@@ -0,0 +1,44 @@
+package ifsc.poo.login;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Register {
+    public static Map<String, String> usuarios = new LinkedHashMap<>();
+
+    //Cadastra novos usuários a partir do login e senha, garantindo que logins não se repitam
+    public boolean addUser(String login, String senha){
+
+        if(!usuarios.containsKey(login)){
+            usuarios.put(login, senha);
+            return true;
+        }
+         return false;
+        }
+    
+    //Remove usuários com base em seu login
+    public boolean removeUser(String login){
+        if(usuarios.containsKey(login)){
+            usuarios.remove(login);
+            return true;
+        }
+        return false;
+    }
+
+    //Autentica usuários
+    public boolean autenticationUser(String login, String senha){
+
+        return usuarios.containsKey(login) && usuarios.get(login).equals(senha);
+    
+    }
+    
+    public void getUsers(){
+        for(String login : usuarios.keySet()){
+            System.out.println(login);
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/ifsc/poo/login/User.java
@@ -0,0 +1,62 @@
+package ifsc.poo.login;
+
+public class User {
+    private String login, senha;
+    final static Register register = new Register(); //todos os usuarios compartilham o mesmo registro
+
+    public User(String login, String senha) {
+        if(setLogin(login) && setSenha(senha) && register.addUser(this.login, this.senha)){
+            System.out.println("Usuário " + login +" adicionado com sucesso!");
+        }else{
+            System.out.println("Já existe um usuário com este nome!");
+        }
+        
+    }
+
+    private boolean setLogin(String login){
+        login = login.toLowerCase();
+
+        //Para o caso do usuario não entrar com um login
+        if(login.isEmpty()){
+            return false;
+        }
+        this.login = login;
+        return true;
+    }
+
+    private boolean  setSenha(String senha){
+        if(senha.isEmpty()){
+            return false;
+        }
+        this.senha = senha;
+        return true;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    }
+
+    public String getLogin(){
+        return this.login;
+    }
+
+    static  void  autenticationUser(String login, String senha){
+        if(register.autenticationUser(login, senha)){
+            System.out.println("Usuário " + login + " autenticado com sucesso!");
+        }else{
+            System.out.println("Usuário ou senha inválidos!");
+        }
+    }
+
+    static void getLogins(){
+        register.getUsers();
+    }
+
+    static void removeUser(String login){
+        if(register.removeUser(login)){
+            System.out.println("Usuário " + login + " removido com sucesso!");
+        }else{
+            System.out.println("Usuário inválido ou inexistente!");
+        }
+    }
+}
